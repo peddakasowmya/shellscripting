78184725f173b631402a5539474995532ec75bfe
Sowmya rani Peddaka shankar reddy <sowmyaranipeddakashankarreddy@Sowmyas-MacBook-Pro.local>
2024-03-14 13:41:35 -0500

    updated the readme file

---
+++ 001-shebang.sh
@@ -0,0 +1,9 @@
+#!/bin/shell
+
+Anything starts with '#' is referred as Comment.
+
+Shebang notation tells us with 'which interpretor-this-script-has-to-be-executed'
+There can only be one interpretor per script and it should always be in the first line of the script.
+
+#Comment
+Apart from first line, any line with '#' is referred as COMMENT.
