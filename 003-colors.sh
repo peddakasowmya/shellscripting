cbcbfe5a11bf7d43382d0f3656fbdce334a36731
Sowmya rani Peddaka shankar reddy <sowmyaranipeddakashankarreddy@Sowmyas-MacBook-Pro.local>
2024-03-14 17:57:27 -0500

    bash auto push

---
+++ 003-colors.sh
@@ -0,0 +1,18 @@
+#1/bin/bash
+
+#Every color in bash has a color code and we can use that code based on our need
+
+#Color      Foreground Code     Background Code
+#Black          30                      40
+#Red	        31	                    41
+#Green	        32	                    42
+#Yellow	        33	                    43  
+#Blue	        34	                    44
+#Magenta	    35	                    45
+#Cyan	        36	                    46
+#Light Gray	    37	                    47
+
+#SYNTAX to print colors
+
+#echo -e "\e[COLOR CODEm I am printing a color \e[0m"
+echo -e "\e[32m I am pritning Green color \e[0m"
