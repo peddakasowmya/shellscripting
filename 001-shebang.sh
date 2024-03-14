1456e9b893057f074fe13520e7ed842a224052a6
Sowmya rani Peddaka shankar reddy <sowmyaranipeddakashankarreddy@Sowmyas-MacBook-Pro.local>
2024-03-14 13:51:02 -0500

    pushing the code

--- 001-shebang.sh
+++ 001-shebang.sh
@@ -1,9 +1,11 @@
 #!/bin/shell
 
-Anything starts with '#' is referred as Comment.
+#Anything starts with '#' is referred as Comment.
 
-Shebang notation tells us with 'which interpretor-this-script-has-to-be-executed'
-There can only be one interpretor per script and it should always be in the first line of the script.
+#Shebang notation tells us with 'which interpretor-this-script-has-to-be-executed'
+#There can only be one interpretor per script and it should always be in the first line of the script.
 
 #Comment
-Apart from first line, any line with '#' is referred as COMMENT.
+#Apart from first line, any line with '#' is referred as COMMENT.
+
+echo Welcome to Bash Scripting
