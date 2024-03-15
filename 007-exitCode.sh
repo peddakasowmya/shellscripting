e85feb7ad562a4150b452f9b41270a9fe7adf9d4
Sowmya rani Peddaka shankar reddy <sowmyaranipeddakashankarreddy@Sowmyas-MacBook-Pro.local>
2024-03-15 00:41:54 -0500

    bash auto push

---
+++ 007-exitCode.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#each and every action in linux has exit code
+# 0-255 is the range of codes
+# anything between 1-255 represents either partial success, partial failure or complete failure
+
+# 0----- Global Success
+#1 - 125 ---- Some failure from the command
+# 125+ -- Systems failure
+
---
+++ 008-quotes.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+""  --> double quotes
+''  --> single quotes when used with speacial variables will make it loose its power
+
+a=100
+echo $?
+echo '$?'
+echo "value of a is $?"
+echo 'value of is $?'
+
