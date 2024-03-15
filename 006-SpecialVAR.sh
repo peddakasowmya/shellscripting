5769cb90ab18f6a8b8d60d25f5509b514f89a5ce
Sowmya rani Peddaka shankar reddy <sowmyaranipeddakashankarreddy@Sowmyas-MacBook-Pro.local>
2024-03-15 00:02:43 -0500

    bash auto push

---
+++ 006-SpecialVAR.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#special variabes are pre-defined variables with specific pre-defined powers
+#special variables are $0 to $9, $@,$$, 
+#we cannot create special variables, we can only use them
+
+a=900
+
+echo "value of a is $a"
+echo "Script name is: $0"
+
+
+
+#$0 : prints the name of the script that we are running
