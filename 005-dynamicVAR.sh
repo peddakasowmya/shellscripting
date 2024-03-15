c75389bb3e4b9c1b85020ea627b0528890a59f4c
Sowmya rani Peddaka shankar reddy <sowmyaranipeddakashankarreddy@Sowmyas-MacBook-Pro.local>
2024-03-14 23:55:01 -0500

    bash auto push

--- 005-dynamicVAR.sh
+++ 005-dynamicVAR.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-DATE="14/MARCH/2024"
-NO_OF_SESSIONS="2"
+#DATE="14/MARCH/2024"
+#NO_OF_SESSIONS="2"  #Thesre are hard coding 
 
+DATE="#(date+%F)"
+NO_OF_SESSIONS="$(who|wc-l)"
 echo -e "Today's date is $DATE"
 echo -e "Total active sessions are: $NO_OF_SESSIONS"
