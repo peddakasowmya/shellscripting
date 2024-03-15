4c1ad0f843c641820bd1c4bcbea0fba16371adcd
Sowmya rani Peddaka shankar reddy <sowmyaranipeddakashankarreddy@Sowmyas-MacBook-Pro.local>
2024-03-14 23:20:29 -0500

    bash auto push

--- 004-variables.sh
+++ 004-variables.sh
@@ -5,9 +5,11 @@
 
 a=10
 b=def
-
+CUSTDATA="shipping"
 #lets print the variables
-#echo a  #prints a
-#echo $a
-#echo -e "\e[34mThe value of b is $b\e[0m"
+echo a  #prints a
+echo $a
+echo -e "\e[34mThe value of b is $b\e[0m"
 echo $a,$b 
+
+rm -rf /data/prod/${CUSTDATA}  ===> /data/prod/shipping #this is equivalent to
