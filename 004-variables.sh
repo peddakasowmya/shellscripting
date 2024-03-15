c009d3fc5e63531ad57317cc975900278aac5125
Sowmya rani Peddaka shankar reddy <sowmyaranipeddakashankarreddy@Sowmyas-MacBook-Pro.local>
2024-03-14 19:21:53 -0500

    bash auto push

--- 004-variables.sh
+++ 004-variables.sh
@@ -7,7 +7,7 @@ a=10
 b=def
 
 #lets print the variables
-echo a  #prints a
-echo $a
-echo -e "\e[34mThe value of b is $b\e[0m"
+#echo a  #prints a
+#echo $a
+#echo -e "\e[34mThe value of b is $b\e[0m"
 echo ${a,b}
