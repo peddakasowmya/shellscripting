cbdd7f86f166c0fc9a43f715ce58ca56d465e011
Sowmya rani Peddaka shankar reddy <sowmyaranipeddakashankarreddy@Sowmyas-MacBook-Pro.local>
2024-03-15 01:02:47 -0500

    bash auto push

--- 009-read.sh
+++ 009-read.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 read -p "Enter your name:" name
-echo "Name of teh person running the script is $name"
+echo "Name of the person running the script is $name"
---
+++ 010-functions.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#There are 4 types of commands available
+#1. Binary
+#2. Aliases
+#3. Shell buil-in 
+#4. Functions
+
