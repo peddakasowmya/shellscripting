92350c27739d0035fe06d5856ec8a1541bec5af5
Sowmya rani Peddaka shankar reddy <sowmyaranipeddakashankarreddy@Sowmyas-MacBook-Pro.local>
2024-03-20 00:00:58 -0500

    bash auto push

--- 010-functions.sh
+++ 010-functions.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
 #There are 4 types of commands available
-#1. Binary
-#2. Aliases
-#3. Shell buil-in 
-#4. Functions
+#1. Binary          (/bin, /sbin)
+#2. Aliases         (shortcuts)
+#3. Shell buil-in   (cd, pwd, ls, exit etc)
+#4. Functions       (can be used when we have common pattern)
 
+f() {
+    echo hello
+}
+
+#how to call a funtion
+
+f       #calling funtion
+echo "Today we are learning functions"
+echo "Lets explore functions"
+f
