9110ce007fb5d485f81167350929154f21cf8569
Sowmya rani Peddaka shankar reddy <sowmyaranipeddakashankarreddy@Sowmyas-MacBook-Pro.local>
2024-03-21 01:50:56 -0500

    bash auto push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -8,12 +8,28 @@ if [ $ID -ne 0 ] ; then
     echo -e "\e[31m The script is expected to run with sudo or a root user\e[0m  \n\t EX: bash scriptName compName"
     exit 1
 fi
+
 echo "Installing Nginx Web Server"
 dnf install nginx -y   &>>  /tmp/frontend.log
+if [ $? -eq 0 ] ; then
+    echo -e "\e[32m Success\e[0m"
+else
+    echo -e "\e[31m Failure \e[0m"
+fi
 
 echo "Enabling the service"
 systemctl enable nginx  &>>  /tmp/frontend.log
+if [ $? -eq 0 ] ; then
+    echo -e "\e[32m Success\e[0m"
+else
+    echo -e "\e[31m Failure \e[0m"
+fi
 
 echo "Starting the web server"
 systemctl start nginx   &>>  /tmp/frontend.log
+if [ $? -eq 0 ] ; then
+    echo -e "\e[32m Success\e[0m"
+else
+    echo -e "\e[31m Failure \e[0m"
+fi
 
