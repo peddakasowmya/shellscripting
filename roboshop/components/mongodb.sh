026472a60eccfed6e0b2ee8711b22b6ad7e79d1e
Sowmya rani Peddaka shankar reddy <sowmyaranipeddakashankarreddy@Sowmyas-MacBook-Pro.local>
2024-03-21 02:27:44 -0500

    bash auto push

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -1,3 +1,38 @@
 #!/bin/bash
 
 echo -e "\e[33m I am mongodb component \e[0m"
+
+ID=$(id -u)
+if [ $ID -ne 0 ] ; then
+    echo -e "\e[31m The script is expected to run with sudo or a root user\e[0m  \n\t EX: bash scriptName compName"
+    exit 1
+fi
+
+COMPONENT="mongodb"
+LOGFILE="/tmp/$1.log"
+MONGO_REPO="https://raw.githubusercontent.com/stans-robot-project/${COMPONENT}/main/mongo.repo"
+
+stat() {
+    if [ $1 -eq 0 ] ; then
+    echo -e "\e[32m Success\e[0m"
+else
+    echo -e "\e[31m Failure \e[0m"
+fi
+}
+
+curl -s -o /etc/yum.repos.d/mongodb.repo $MONGO_REPO
+stat $?
+
+echo -e "\e[35m **********__$COMPONENT configuration is Started___************\e[0m"
+
+echo -n "Installing MONGODB Server"
+dnf install -y mongodb-org   &>>  LOGFILE
+stat $?
+
+echo -n "Enabling the MONGODB"
+systemctl enable mongod     &>>  LOGFILE
+stat $?
+
+echo -n "Starting the MONGODB"
+systemctl start mongod      &>>  LOGFILE
+stat $?
