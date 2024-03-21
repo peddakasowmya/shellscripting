dbd138d1fc3cf11fdf2600e468a814ee3de02727
Sowmya rani Peddaka shankar reddy <sowmyaranipeddakashankarreddy@Sowmyas-MacBook-Pro.local>
2024-03-20 23:51:01 -0500

    bash auto push

---
+++ 016-loops.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Loops: to run something for required number of times
+<<COMMENT
+There are 2 types of loops
+1. For loop     (when you know how many times to be executed)
+2. While loop   (when you do not know how many times to be executed)
+
+for x in a b c d e ; do
+echo print $x
+done
+
+COMMENT
+
+for x in 10 20 30 40 50 ; do
+    echo " values are $x \n\t"
+done
