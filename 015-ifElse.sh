9ace18698ccd66cbd6927f15f766694ac5aa07f3
Sowmya rani Peddaka shankar reddy <sowmyaranipeddakashankarreddy@Sowmyas-MacBook-Pro.local>
2024-03-20 23:25:39 -0500

    bash auto push

--- 015-ifElse.sh
+++ 015-ifElse.sh
@@ -4,7 +4,9 @@ echo -e "Demo on if, if-else and else-if conditions"
 ACTION=$1
 
 if [ "$ACTION" = "start" ] ; then
+    echo  -e "\e[32m starting dispatch Service \e[0m"
 
-echo  -e "\e[32m starting dispatch Service \e[0m"
+else
+    echo  -e "\e[32m Only start option is available \e[0m"
 
 fi
