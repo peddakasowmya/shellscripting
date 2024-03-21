1a34e179767f32a369cdea9c43d9eb9298318509
Sowmya rani Peddaka shankar reddy <sowmyaranipeddakashankarreddy@Sowmyas-MacBook-Pro.local>
2024-03-20 23:28:27 -0500

    bash auto push

--- 015-ifElse.sh
+++ 015-ifElse.sh
@@ -6,7 +6,13 @@ ACTION=$1
 if [ "$ACTION" = "start" ] ; then
     echo  -e "\e[32m starting dispatch Service \e[0m"
 
+elif [ "$ACTION" = "stop" ] ; then
+    echo -e "\e[31m stopping Dispatch Service \e[0m"
+
+elif [ "$ACTION" = "restart" ] ; then
+    echo -e "\e[33m restarting Dispatch Service \e[0m"
+
 else
-    echo  -e "\e[39m Only start option is available \e[0m"
+    echo  -e "\e[37m Only start option is available \e[0m"
 
 fi
