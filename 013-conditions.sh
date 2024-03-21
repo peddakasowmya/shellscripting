c2fa7a692aebe1c8354b9e671b9429dd337647b4
Sowmya rani Peddaka shankar reddy <sowmyaranipeddakashankarreddy@Sowmyas-MacBook-Pro.local>
2024-03-20 19:17:45 -0500

    bash auto push

--- 013-conditions.sh
+++ 013-conditions.sh
@@ -14,12 +14,15 @@ ACTION=$1
 case $ACTION in
     start)
         echo  -e "\e[32m starting dispatch Service \e[0m"
+        exit 0
         ;;
     stop)
         echo -e "\e[31m stopping Dispatch Service \e[0m"
+        exit 1
         ;;
     restart)
         echo -e "\e[33m restarting Dispatch Service \e[0m"
+        exit 3
         ;;
     *)
         echo -e "\e[34m Valid options are: start, stop and restart only \e[0m"
