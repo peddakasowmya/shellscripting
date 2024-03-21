822ab68a99f12a24c0ed3d4e519b97c7c8576080
Sowmya rani Peddaka shankar reddy <sowmyaranipeddakashankarreddy@Sowmyas-MacBook-Pro.local>
2024-03-21 01:45:05 -0500

    bash auto push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -4,9 +4,9 @@
 #Check whether you are running this script with SUDO or root user or not. If not exit the user
 
 ID=$(id -u)
-if [ $ID -ne 0] ; then
-echo -e "\e[31m The script is expected to run with sudo \e[0m  \n\t EX: bash scriptName compName"
-exit 1
+if [ $ID -ne 0 ] ; then
+    echo -e "\e[31m The script is expected to run with sudo \e[0m  \n\t EX: bash scriptName compName"
+    exit 1
 fi
 echo "Installing Nginx Web Server"
 dnf install nginx -y 
