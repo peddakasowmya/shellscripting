a5dcb1aad4a3cb42505ce811fb9682cc08b30013
Sowmya rani Peddaka shankar reddy <sowmyaranipeddakashankarreddy@Sowmyas-MacBook-Pro.local>
2024-03-20 19:06:02 -0500

    bash auto push

---
+++ 013-conditions.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#conditions helps us to execute something only if some fator is true
+
+<<CASE
+#Syntax of case is:
+case $var in
+    opt1) commands-x ;;
+    opt2) commands-y ;;
+esac
+CASE
+
+case $ACTION in
+    start)
+    echo "starting dispatch Service"
+    ;;
+    stop)
+    echo "stopping Dispatch Service"
+    ;;
+    restart)
+    echo "restarting Dispatch Service"
+    ;;
+
+esac
+
