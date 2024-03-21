3e69e8200bde0b5424cde7369d19ee58fd689976
Sowmya rani Peddaka shankar reddy <sowmyaranipeddakashankarreddy@Sowmyas-MacBook-Pro.local>
2024-03-21 02:13:03 -0500

    bash auto push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -20,6 +20,7 @@ fi
 
 }
 
+echo -e "\e[32m **********__$COMPONENT configuration is Started___************\e[0m"
 
 echo -n "Installing Nginx Web Server"
 dnf install nginx -y   &>>  LOGFILE
@@ -56,3 +57,6 @@ stat $?
 echo -n "Restarting the web server"
 systemctl restart nginx   &>>  LOGFILE
 stat $?
+
+echo -e "\e[32m *********____$COMPONENT configuration is Completed___************\e[0m"
+
