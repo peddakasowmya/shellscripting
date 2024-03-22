9bbea0172e1c287745a45e2864ea01f9a00387d7
Sowmya rani Peddaka shankar reddy <sowmyaranipeddakashankarreddy@Sowmyas-MacBook-Pro.local>
2024-03-21 22:32:03 -0500

    bash auto push

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -55,7 +55,7 @@ stat $?
 echo -n "Extracting the $COMPONENT: "
 cd /home/roboshop
 unzip -o /tmp/${COMPONENT}.zip        &>> $LOGFILE
-mv /home/${APPUSER}/${COMPONENT}-main ${APPUSER_DIR}/${COMPONENT}  &>> $LOGFILE
+mv /home/${APPUSER}/${COMPONENT}-main /home/${APPUSER}/${COMPONENT} 
 stat $?
 
 echo -n "Configuring permission : "
