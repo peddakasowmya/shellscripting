6e66a0fc794614c535c1f7339dcdf952326a6682
Sowmya rani Peddaka shankar reddy <sowmyaranipeddakashankarreddy@Sowmyas-MacBook-Pro.local>
2024-03-22 22:16:00 -0500

    bash auto push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -55,7 +55,7 @@ mv localhost.conf /etc/nginx/default.d/roboshop.conf
 stat $?
 
 echo -n "Updating the reverse proxy file:"
-    for i in catalogue user ; do
+    for i in catalogue user cart ; do
         sed -i -e "/$i/s/localhost/$i.roboshop.internal/" /etc/nginx/default.d/roboshop.conf
     done 
 stat $?
