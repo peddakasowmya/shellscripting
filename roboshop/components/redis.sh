fde3ae63454384eaf46537815529bad94df55760
Sowmya rani Peddaka shankar reddy <sowmyaranipeddakashankarreddy@Sowmyas-MacBook-Pro.local>
2024-03-26 13:48:21 -0500

    bash auto push

--- roboshop/components/redis.sh
+++ roboshop/components/redis.sh
@@ -2,23 +2,11 @@
 
 echo -e "\e[33m I am redis component \e[0m"
 
-ID=$(id -u)
-if [ $ID -ne 0 ] ; then
-    echo -e "\e[31m The script is expected to run with sudo or a root user\e[0m  \n\t EX: bash scriptName compName"
-    exit 1
-fi
 COMPONENT="redis"
 LOGFILE="/tmp/$1.log"
 REDIS_REPO="https://rpms.remirepo.net/enterprise/remi-release-8.rpm"
 
-stat() {
-    if [ $1 -eq 0 ] ; then
-    echo -e "\e[32m Success\e[0m"
-else
-    echo -e "\e[31m Failure \e[0m"
-fi
-
-}
+source components/common.sh  
 
 echo -e "\e[35m **********__$COMPONENT configuration is Started___*******\e[0m"
 
