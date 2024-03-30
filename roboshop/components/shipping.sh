b54398632bd66a8178af2b2c9e9f7129d1a3574a
Sowmya rani Peddaka shankar reddy <sowmyaranipeddakashankarreddy@Sowmyas-MacBook-Pro.local>
2024-03-29 23:35:00 -0500

    bash auto push

--- roboshop/components/shipping.sh
+++ roboshop/components/shipping.sh
@@ -4,15 +4,11 @@ echo -e "\e[33m I am catalogue component \e[0m"
 
 COMPONENT="shipping"
 
-
 source components/common.sh  
 
-echo -e "\e[35m ********__$COMPONENT configuration is Started___************\e[0m"
-
-
+echo -e "\e[35m ********__$COMPONENT configuration is Started___*******\e[0m"
 #calling NoseJS function
 MAVEN
 
-
-echo -e "\e[35m **********__$COMPONENT configuration is Started___************\e[0m"
+echo -e "\e[35m ********__$COMPONENT configuration is Started___********\e[0m"
 
