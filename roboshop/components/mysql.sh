9b5c03be79e95d2d5b38f74344e0eea9a99a4001
Sowmya rani Peddaka shankar reddy <sowmyaranipeddakashankarreddy@Sowmyas-MacBook-Pro.local>
2024-03-29 23:36:08 -0500

    bash auto push

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -1,68 +1,58 @@
-#!/bin/bash
+#!/bin/bash 
 
-echo -e "\e[33m I am MySQL component \e[0m"
+COMPONENT=mysql
 
-COMPONENT="mysql"
-# LOGFILE="/tmp/${COMPONENT}.log"
-MYSQL_REPO="https://raw.githubusercontent.com/stans-robot-project/${COMPONENT}/main/${COMPONENT}.repo"
-SCHEMA_URL="https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
+source components/common.sh
 
-source components/common.sh         #source will keep all the functions local to the current script that declared in other file
-
-echo -e "\e[35m **********__$COMPONENT configuration is Started___************\e[0m"
-
-# echo -n "Disabling $COMPONENT repository: "
-# dnf module disable mysql -y     &>>  $LOGFILE
-# stat $?
+echo -e "\e[35m Configuring ${COMPONENT} ......! \e[0m \n"
 
 echo "Disabling $COMPONENT repo :"
 dnf module disable mysql -y     &>>  $LOGFILE
 stat $? 
 
-echo -n "Configuring $COMPONENT repository"
-curl -s -L -o /etc/yum.repos.d/mysql.repo $MYSQL_REPO
+echo -n "Configuring ${COMPONENT} repo :"
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/stans-robot-project/mysql/main/mysql.repo
 stat $?
 
-echo -n "Installing $COMPONENT Server"
+echo -n "Installing ${COMPONENT}  :"
 dnf install mysql-community-server -y     &>>  ${LOGFILE}
 stat $?
 
-echo -n "Starting $COMPONENT :"
+echo -n "Starting ${COMPONENT}:" 
 systemctl enable mysqld   &>>  ${LOGFILE}
 systemctl start mysqld    &>>  ${LOGFILE}
 stat $?
 
-echo -n "Extracting the default root password:"
+echo -n "Extracting the default mysql root password :"
 export DEFAULT_ROOT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk -F " " '{print $NF}')
-stat $?
+stat $? 
 
-echo "show databases;" | mysql -uroot -p${mysql_root_password}   &>> $LOGFILE
-if [ $? -ne 0 ] ; then
-    echo -n "Changing $COMPONENT root password:"
-    echo "ALTER USER 'root@'location' IDENTIFIED BY ${mysql_root_password}" | mysql --connect-expired-password -uroot -p$DEFAULT_ROOT_PASSWORD
+# This should happen only once and that too for the first time, when it runs for the second time, jobs fails.
+# We need to ensure that this runs only once.
+echo "show databases;" | mysql -uroot -p${mysql_root_password} &>>  ${LOGFILE}
+if [ $? -ne 0 ]; then 
+    echo -n "Performing default password reset of root account:"
+    echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1'" | mysql  --connect-expired-password -uroot -p$DEFAULT_ROOT_PASSWORD &>>  ${LOGFILE}
     stat $?
-fi
+fi 
 
-echo "show plugins;" | mysql -uroot -p${mysql_root_password} | grep validate_password  &>> $LOGFILE
-if [ $? -eq 0 ] ; then
-    echo -n "Uninstalling plugins"
-    echo "uninstall plugin validate_password" | mysql -uroot -p${mysql_root_password}
+echo "show plugins;" | mysql -uroot -p${mysql_root_password} | grep validate_password  &>>  ${LOGFILE}
+if [ $? -eq 0 ]; then 
+    echo -n "Uninstalling Password-validate plugin :"
+    echo "uninstall plugin validate_password" | mysql -uroot -p${mysql_root_password} &>>  ${LOGFILE}
     stat $?
+fi 
 
-fi
+echo -n "Downloading the $COMPONENT schema:"
+curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip"
+stat $? 
 
-echo -n "Downloading the $COMPONENT component: "
-curl -s -L -o /tmp/${COMPONENT}.zip ${SCHEMA_URL}       &>> $LOGFILE
+echo -n "Extracting the $COMPONENT Schema:"
+cd /tmp  
+unzip -o /tmp/${COMPONENT}.zip   &>> $LOGFILE
+stat $? 
+
+echo -n "Injecting the schema "
+cd ${COMPONENT}-main 
+mysql -u root -p${mysql_root_password} <shipping.sql     &>>  ${LOGFILE} 
 stat $?
-
-echo -n "Extracting the $COMPONENT component :"
-cd /tmp
-unzip -o /tmp/mysql.zip                             &>> $LOGFILE
-stat $?
-
-echo -n "Injecting the schema"
-cd ${COMPONENT}-main
-mysql -u root -p${mysql_root_password} <shipping.sql    &>> $LOGFILE
-stat $?
-
-echo -e "\e[35m ********__$COMPONENT configuration is Completed___********\e[0m"
