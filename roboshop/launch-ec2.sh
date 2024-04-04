ad261e62359a69998d1fc0bc83cfc9f1819afb74
Sowmya rani Peddaka shankar reddy <sowmyaranipeddakashankarreddy@Sowmyas-MacBook-Pro.local>
2024-04-03 23:24:28 -0500

    bash auto push

--- roboshop/launch-ec2.sh
+++ roboshop/launch-ec2.sh
@@ -7,12 +7,12 @@ SGID="sg-097ec9c3903099238"  #Security group ID
 COMPONENT=$1
 
 if [ -z $1 ] ; then
-echo -e "\e[31m COMPONENT NAME IS NEEDED: \e[0m"
-echo -e "\e[31m \t\t Exaple Usage :  \e[0m bash launch-ec2 ratings"
-exit 1
+    echo -e "\e[31m COMPONENT NAME IS NEEDED: \e[0m"
+    echo -e "\e[31m \t\t Exaple Usage :  \e[0m bash launch-ec2 ratings"
+    exit 1
 fi
 
-aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SGID --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]' 
+aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SGID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]"
 
 # 'ResourceType=volume,Tags=[{Key=cost-center,Value=cc123}]'
 
