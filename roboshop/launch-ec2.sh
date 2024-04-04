57fbc22c49daf3ae654b5aaba6df8f0200552852
Sowmya rani Peddaka shankar reddy <sowmyaranipeddakashankarreddy@Sowmyas-MacBook-Pro.local>
2024-04-04 14:12:58 -0500

    bash auto push

--- roboshop/launch-ec2.sh
+++ roboshop/launch-ec2.sh
@@ -6,6 +6,9 @@ AMI_ID="ami-072983368f2a6eab5"
 SGID="sg-097ec9c3903099238"  #Security group ID
 COMPONENT=$1
 HOSTEDZONE_ID="Z00324643QL0XDPZAHOTX"  #our own hosted zone ID
+COLOR="\e[35m"
+NOCOLOR="\e[0m"
+ENV=$2
 
 if [ -z $1 ] ; then
     echo -e "\e[31m COMPONENT NAME IS NEEDED: \e[0m"
@@ -14,11 +17,11 @@ if [ -z $1 ] ; then
 fi
 
 PRIVATE_IP=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t2.micro --security-group-ids $SGID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq .Instances[].PrivateIpAddress | sed -e 's/"//g')
-echo "$1 Server is created and here is the IP Address : $PRIVATE_IP"  
+echo "$COLOR $1 Server is created and here is the IP Address : $PRIVATE_IP  $NOCOLOR"  
 
-echo -e "Creating r53 json file with component name and IP address: "
+echo -e "$COLOR Creating r53 json file with component name and IP address:  $NOCOLOR "
 sed -e "s/IPADDRESS/${PRIVATE_IP}/g" -e "s/COMPONENT/${COMPONENT}/g" route53.json > /tmp/dns.json
 
-echo "Creating DNS record for $COMPONENT :"
+echo " $COLOR Creating DNS record for $COMPONENT :   $NOCOLOR"
 aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONE_ID --change-batch file:///tmp/dns.json
 
